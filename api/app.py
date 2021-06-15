1988b9aefc23a8a6d2abdd8078e6243b8b957e66
phantuanminh <phantuanminh2002@gmail.com>
2021-06-16 05:53:07 +0700

    Add Kafka/Zookeeper and Python Producer

--- api/app.py
+++ api/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+from kafka import KafkaProducer
+from time import sleep
 
 app = Flask(__name__)
 
@@ -9,7 +11,15 @@ CORS(app, supports_credentials=True)
 
 @app.route('/api/message', methods=['POST', 'GET', 'OPTIONS'])
 def message():
+    # Get data from request
     data = request.get_json()
+
+    # Initiate Producer
+    producer = KafkaProducer(bootstrap_servers=['192.168.1.40:9091'])
+    producer.send(topic=data['topic'],
+                  partition=data['partition'], value=data['message'].tobytes())
+    sleep(0.5)
+
     return jsonify({'result': 'fail'})
 
 
